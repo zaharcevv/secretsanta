d5d83a5ed5cc0adcf68d1a8f913b78330099d260
Hristijan <hristijanzaharcev1@gmail.com>
2024-12-26 00:49:20 +0100

    final

---
+++ main.js
@@ -0,0 +1,6 @@
+document.getElementById('revealBtn').addEventListener('click', function() {
+    var hiddenContent = document.getElementById('hiddenContent');
+    hiddenContent.style.display = 'block'; // Show the hidden content
+});
+
+  
